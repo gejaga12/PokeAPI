f919372056df04ce5e0afa9c63006287090d5c92
Gerardo Javier García <gejaga12@hotmail.com>
2022-06-24 15:43:25 -0300

    Agregado de btn atras/sig

--- pruebaApi.js
+++ pruebaApi.js
@@ -4,43 +4,52 @@ let obtenerHabilidades = document.getElementById("habilidades");
 let pokefoto = document.getElementById("pokefoto");
 let pokefotoAtras = document.getElementById("pokefoto");
 let form = document.forms["form"];
+let pokemonId = 151;
+
 let pokeFotoAtras =
   "https://img.pokemondb.net/sprites/black-white/anim/back-normal/onix.gif";
 let pokeFotoFrente =
   "https://img.pokemondb.net/sprites/black-white/anim/normal/onix.gif";
+const pokeSig = document.getElementById("ObtenerId");
 
 // TRAER EL VALOR INGRESADO EN EL FORMULARIO
 form.onsubmit = function (e) {
   e.preventDefault();
-  let pokemonId = document.form.nombrePokemon.value.toLowerCase();
-  buscarPokemon(pokemonId);
+  let pokemonName = document.form.nombrePokemon.value.toLowerCase();
+  buscarPokemon(pokemonName);
 };
 
 // DESDE LA API BUSCAMOS POKEMON CON NOMBRE O ID INGRESADO EN EL FORMULARIO
-function buscarPokemon(pokemonId) {
-  fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonId)
-  .then(response => {
-    if (!response.ok) throw Error(response.status);
-
-    return response;
-})
+function buscarPokemon(pokemonName) {
+  fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
+    .then((response) => {
+      if (!response.ok) throw Error(response.status);
+      return response;
+    })
     .then((response) => response.json())
-    .catch(error => alert("No se encuentra el pokemon "+pokemonId+" eso no es muy pokeamigo de tu parte"))
+    .catch((error) =>
+      alert(
+        "No se encuentra el Pokemon " +
+          pokemonName +
+          " eso no es muy Pokeamigo de tu parte"
+      )
+    )
     .then((pokemon) => {
-      pokemonId = pokemon.name;
+      pokemonName = pokemon.name;
+      pokemonId = pokemon.id;
       pokeFotoFrente =
         "https://img.pokemondb.net/sprites/black-white/anim/normal/" +
-        pokemonId +
+        pokemonName +
         ".gif";
       pokeFotoAtras =
         "https://img.pokemondb.net/sprites/black-white/anim/back-normal/" +
-        pokemonId +
+        pokemonName +
         ".gif";
       pokefoto.setAttribute("src", pokeFotoFrente);
 
-      obtenerNombre.innerHTML = pokemonId.toUpperCase();
+      obtenerNombre.innerHTML = pokemonName.toUpperCase();
 
-      let obtenerId = document.getElementById("ObtenerId");
+      let obtenerId = document.getElementById("ObtenerId"); // obtengo la id
 
       while (obtenerId.hasChildNodes()) {
         obtenerId.removeChild(obtenerId.firstChild);
@@ -58,11 +67,10 @@ function buscarPokemon(pokemonId) {
           listaOrdenadaHabilidades.firstChild
         );
       }
-
       pokemon.moves.map((nombreHabilidad, index) => {
         if (index < 4) {
           let textoNombreHabilidad = document.createTextNode(
-            "•  " + nombreHabilidad.move.name
+            nombreHabilidad.move.name
           );
           let crearNombreHabilidad = document.createElement("li");
           crearNombreHabilidad.appendChild(textoNombreHabilidad);
@@ -70,14 +78,12 @@ function buscarPokemon(pokemonId) {
           listaOrdenadaHabilidades.appendChild(crearNombreHabilidad);
         }
       });
-    })
-    
+    });
 }
 
 let estadoFoto = false;
 
 $("#pokefoto").click(function () {
-  console.log(estadoFoto);
   if (estadoFoto == true) {
     $("#pokefoto").attr("src", pokeFotoFrente);
     estadoFoto = false;
@@ -85,4 +91,23 @@ $("#pokefoto").click(function () {
     $("#pokefoto").attr("src", pokeFotoAtras);
     estadoFoto = true;
   }
+});
+
+$(".fakeimg").hover(
+  function () {
+    $(this).addClass("grande");
+  },
+  function () {
+    $(this).removeClass("grande");
+  }
+);
+
+$("#anterior").click(function () {
+  pokemonId--;
+  buscarPokemon(pokemonId);
+});
+
+$("#siguiente").click(function () {
+  pokemonId++;
+  buscarPokemon(pokemonId);
 });
