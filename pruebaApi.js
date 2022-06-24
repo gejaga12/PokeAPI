eed301337d045fa7ed789a365b0821dbb00d0e46
Gerardo Javier García <gejaga12@hotmail.com>
2022-06-24 09:59:04 -0300

    solucion de bug buscar con ID

--- pruebaApi.js
+++ pruebaApi.js
@@ -1,3 +1,4 @@
+// OBTENER ELEMENTOS DEL DOM
 let obtenerNombre = document.getElementById("nombre");
 let obtenerHabilidades = document.getElementById("habilidades");
 let pokefoto = document.getElementById("pokefoto");
@@ -8,29 +9,30 @@ let pokeFotoAtras =
 let pokeFotoFrente =
   "https://img.pokemondb.net/sprites/black-white/anim/normal/onix.gif";
 
+// TRAER EL VALOR INGRESADO EN EL FORMULARIO
 form.onsubmit = function (e) {
   e.preventDefault();
-  let seleccion = document.form.nombrePokemon.value.toLowerCase();
-  pokemonId = seleccion;
-  buscarPokemon();
+  let pokemonId = document.form.nombrePokemon.value.toLowerCase();
+  buscarPokemon(pokemonId);
 };
 
-function buscarPokemon() {
-  pokeFotoFrente= "https://img.pokemondb.net/sprites/black-white/anim/normal/" + pokemonId + ".gif"
-  pokeFotoAtras=  "https://img.pokemondb.net/sprites/black-white/anim/back-normal/"+ pokemonId +".gif";
-  pokefoto.setAttribute("src", pokeFotoFrente);
-
-    fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonId)
+// DESDE LA API BUSCAMOS POKEMON CON NOMBRE O ID INGRESADO EN EL FORMULARIO
+function buscarPokemon(pokemonId) {
+  fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonId)
     .then((response) => response.json())
     .then((pokemon) => {
+      pokemonId = pokemon.name;
+      pokeFotoFrente =
+        "https://img.pokemondb.net/sprites/black-white/anim/normal/" +
+        pokemonId +
+        ".gif";
+      pokeFotoAtras =
+        "https://img.pokemondb.net/sprites/black-white/anim/back-normal/" +
+        pokemonId +
+        ".gif";
+      pokefoto.setAttribute("src", pokeFotoFrente);
 
-      // pokefoto.setAttribute(
-      //   "src",
-      //   pokemon.sprites["other"]["official-artwork"]["front_default"]
-      // );
-
-      obtenerNombre.innerHTML = pokemonId;
-      let textoNombre = document.createTextNode(pokemon.name.toUpperCase());
+      obtenerNombre.innerHTML = pokemonId.toUpperCase();
 
       let obtenerId = document.getElementById("ObtenerId");
 
@@ -70,10 +72,10 @@ let estadoFoto = false;
 $("#pokefoto").click(function () {
   console.log(estadoFoto);
   if (estadoFoto == true) {
-    $("#pokefoto").attr("src",pokeFotoFrente );
+    $("#pokefoto").attr("src", pokeFotoFrente);
     estadoFoto = false;
   } else {
-    $("#pokefoto").attr("src",pokeFotoAtras );
+    $("#pokefoto").attr("src", pokeFotoAtras);
     estadoFoto = true;
   }
 });
